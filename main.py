ca995cfb5738daf60a19692e66ffdd82739393d0
NAgato1231 <nicer1@mail.ru>
2021-10-12 20:38:28 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,78 @@
+a = 2
+dateOfBirthday = 1994  # Camel case
+date_of_birthday = 1993  # Snake case
+var_int = 2  # Variable integer
+var_str = "Hello iman"  # Variable string(строка)
+var_boolean_true = True  # Variable boolean(Истина)
+var_boolean_false = False  # Variable boolean(Лож)
+var_list = [2, 4, 5, "Hello iman",[2, 4, 5], (2,4,5) ]  # Variable list(список)
+var_tuple = (2,[2, 3], "Hello iman")
+var_dict = {"name": "hello iman", "date_of_birthday": "02.05.2003", "course": 3,
+            "physical_data": [90, 100], "python": True}  # Variable dictionary(словарь)
+
+
+def print_hi(var_int):
+    var_1 = var_int
+    var_int = 2
+    print(var_1, var_int)
+    var_str = "Hello iman"  # Variable string(строка)
+    var_boolean_true = True  # Variable string(строка)
+    var_boolean_fale = False  # Variable boolean(Лож)
+    var_list = [2, 4, 5, "Hello iman", [2, 4, 5], (2,4,5) ]  # Variable list (Список)
+    var_tuple = (2,[2, 3], "Hello iman")
+    var_dict = {"name": "hello iman", "date_of_birthday": "02.05.2003", "course":3,
+                "physical_data": [90, 100], "python":True}  # Variable dictionary(словарь)
+
+
+
+if __name__ == '__main__':
+    print("This is your homework")
+    print(var_int)
+    print(var_str)
+    print(var_boolean_true)
+    print(var_boolean_false)
+    print(var_list)
+    print(var_tuple)
+    print(var_dict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # a = 2
+    # b = 3
+    # print("This is your homework")
+    # digit = 18
+    # list_of_digits = [2 + 1]
+    #
+    # print(list_of_digits)
+    #
+    # list_of_digits = [3 + 2]
+    #
+    # print(list_of_digits)
+    # print(int("1000") - int("7"))
+    # print(int("2") < int("2"))
+    # print(int("2") > int("1"))
+    # print(int("3") > int("3"))
+    # print(int("3") > int("2"))
+    # print(type(list_of_digits))
